6eb380f0f810be3f89f116fc7f658218902230f2
Matt Zukowski <matt@zukowski.ca>
2011-09-29 00:03:20 -0400

    added notetaker agent to save common knowledge notes to the database

---
+++ assets/www/agents/notetaker.rb
@@ -0,0 +1,51 @@
+require 'rubygems'
+require 'blather/client/dsl'
+require 'json'
+require 'mongo'
+
+$: << 'sail.rb/lib'
+require 'sail/agent'
+
+class Notetaker < Sail::Agent  
+  def behaviour
+    when_ready do
+      @mongo = Mongo::Connection.new.db(config[:database])
+      
+      pres = Blather::Stanza::Presence::Status.new
+      pres.to = agent_jid_in_room
+      pres.state = :chat
+      
+      log "Joining #{agent_jid_in_room.inspect}..."
+      
+      client.write(pres)
+    end
+    
+    event :ck_new_note? do |stanza, sev|
+      note = {
+        :content => sev['payload'],
+        :author  => sev['origin'],
+        :timestamp => sev['timestamp']
+      }
+      
+      store_note_in_mongo(note)
+    end
+    
+    message :error? do |err|
+      log err, :ERROR
+    end
+    
+    disconnected do
+      # automatically reconnect
+      log "disconnected... will attempt to reconnect", :WARN
+      client.connect
+    end
+  end
+  
+  
+  protected
+  
+  def store_note_in_mongo(note)
+    log "Storing note: #{note.inspect}"
+    @mongo.collection('notes').save(note)
+  end
+end
