7548c93af4858cfd471966c46ac95c4e31970d71
Armin Krauss <mackrauss@gmail.com>
2011-09-30 16:05:19 -0400

    Determine batch size before find by doing a count. Started in generateRelationshipDT - having to do it for all find

--- assets/www/js/wallcology.js
+++ assets/www/js/wallcology.js
@@ -613,7 +613,51 @@ WallCology = {
 		},
 
 		generateRelationshipsDT: function(from, to) {
-			$.get("/mongoose/wallcology/observations/_find", { criteria: JSON.stringify({"type":"relationship", "energy_transfer.from":from, "energy_transfer.to":to}), batch_size: 200 },
+			$.get("/mongoose/wallcology/observations/_count",
+				{ criteria: JSON.stringify({"type":"relationship", "energy_transfer.from":from, "energy_transfer.to":to})},
+				function(data) {
+			    	if (data.ok === 1) {			    		
+						batchSize = data.count
+						$.get("/mongoose/wallcology/observations/_find",
+							{ criteria: JSON.stringify({"type":"relationship", "energy_transfer.from":from, "energy_transfer.to":to}), batch_size: batchSize },
+							function(data) {
+								relationshipResultsArray = []
+								for (i=0;i<data.results.length;i++) {
+									d = new Date(data.results[i].timestamp)
+									relationshipResultsArray[i] = [data.results[i].comments, data.results[i].origin, Sail.app.observations.dateString(d)]
+								}
+
+						    	if (data.ok === 1) {			    		
+									$('#relationships-datatable').dataTable({
+										"iDisplayLength": 6,
+										"bLengthChange": false,
+										"bDestroy" : true,		//you need this so that the table will be refreshed without errors each time entering the page
+										"bJQueryUI": true,
+										"sPaginationType": "full_numbers",
+										"aoColumns": [        
+														{ "sWidth": "500px" },
+														null,
+														null
+													],
+
+										"aaData": relationshipResultsArray	
+									})
+						    	}
+						    	else {
+									console.log("Mongoose request failed")
+									return false
+								}
+						}, "json")
+			    	}
+			    	else {
+						console.log("Mongoose request failed")
+						return false
+					}
+			}, "json")
+			
+			
+			/*$.get("/mongoose/wallcology/observations/_find",
+				{ criteria: JSON.stringify({"type":"relationship", "energy_transfer.from":from, "energy_transfer.to":to}), batch_size: 200 },
 				function(data) {
 					relationshipResultsArray = []
 					for (i=0;i<data.results.length;i++) {
@@ -641,7 +685,7 @@ WallCology = {
 						console.log("Mongoose request failed")
 						return false
 					}
-			}, "json")
+			}, "json")*/
 		},
 
 
