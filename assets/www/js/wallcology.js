96f14cc025bd19bd7474518808e5c9610fa69854
Matt Zukowski <matt@zukowski.ca>
2011-10-06 09:09:14 -0400

    xmppDomain should be aardvark.encorelab.org

--- assets/www/js/wallcology.js
+++ assets/www/js/wallcology.js
@@ -1,7 +1,7 @@
 WallCology = {
     rollcallURL: '/rollcall', //'http://rollcall.proto.encorelab.org',
 	mongooseURL: '/mongoose',
-    xmppDomain: 'proto.encorelab.org',
+    xmppDomain: 'aardvark.encorelab.org',
     groupchatRoom: null,
     
     // all Sail events generated in this app must have a run
