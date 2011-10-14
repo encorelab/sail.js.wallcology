aa83812bc65bcaec493ba32e1169946f7312d171
Matt Zukowski <matt@zukowski.ca>
2011-10-14 13:07:52 -0400

    don't need xmppDomain and groupchatRoom at the top anymore since these are set automatically

--- assets/www/js/wallcology.js
+++ assets/www/js/wallcology.js
@@ -1,8 +1,6 @@
 WallCology = {
     rollcallURL: '/rollcall', //'http://rollcall.proto.encorelab.org',
     mongooseURL: '/mongoose',
-    xmppDomain: null,
-    groupchatRoom: null,
     
     // all Sail events generated in this app must have a run
     allowRunlessEvents: false,
