e024afcd2f84a282a6438f31fa79b1a0b73d1113
Matt Zukowski <matt@zukowski.ca>
2011-10-14 14:33:32 -0400

    agents are now configured using config.json

--- assets/www/agents/daemon.rb
+++ assets/www/agents/daemon.rb
@@ -5,7 +5,6 @@ require 'archivist'
 require 'notetaker'
 
 AGENT_PASSWORD = "1d6f760bc95729166e551d7bee1d75c69b133015"
-XMPP_DOMAIN = `hostname`.strip # assuming that the current hostname is also the xmpp domain
 
 @daemon = Sail::Daemon.spawn(
   :name => "wallcology",
@@ -13,12 +12,14 @@ XMPP_DOMAIN = `hostname`.strip # assuming that the current hostname is also the 
   :verbose => true
 )
 
+@daemon.load_config("../config.json")
+
 # Julia's run
-@daemon << Archivist.new(:room => "wallcology-julia-fall2011", :host => XMPP_DOMAIN, :password => AGENT_PASSWORD, :database => 'wallcology')
-@daemon << Notetaker.new(:room => "wallcology-julia-fall2011", :host => XMPP_DOMAIN, :password => AGENT_PASSWORD, :database => 'common-knowledge')
+@daemon << Archivist.new(:room => "wallcology-julia-fall2011", :password => AGENT_PASSWORD, :database => 'wallcology')
+@daemon << Notetaker.new(:room => "wallcology-julia-fall2011", :password => AGENT_PASSWORD, :database => 'common-knowledge')
 
 # Ben's run
-@daemon << Archivist.new(:room => "wallcology-ben-fall2011", :host => XMPP_DOMAIN, :password => AGENT_PASSWORD, :database => 'wallcology')
-@daemon << Notetaker.new(:room => "wallcology-ben-fall2011", :host => XMPP_DOMAIN, :password => AGENT_PASSWORD, :database => 'common-knowledge')
+@daemon << Archivist.new(:room => "wallcology-ben-fall2011", :password => AGENT_PASSWORD, :database => 'wallcology')
+@daemon << Notetaker.new(:room => "wallcology-ben-fall2011", :password => AGENT_PASSWORD, :database => 'common-knowledge')
 
 @daemon.start
