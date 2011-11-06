59d65f9bafb5434ec3c299dda59d254b540043c0
Matt Zukowski <matt@zukowski.ca>
2011-11-06 01:08:34 -0500

    added hard-coded proxy rule for UIC

--- assets/www/server.js
+++ assets/www/server.js
@@ -1,3 +1,23 @@
 var sail = require('./js/sail.js/sail.node.server.js')
 
+var url = require('url')
+
+var UIC_MICROWORLDS_URL = "http://ltg.evl.uic.edu:80/"
+
+sail.server.proxyMap.unshift(
+    {
+        name: 'UIC-MICROWORLDS',
+        match: function(req) { return url.parse(req.url).pathname.match(/^\/uic/) },
+        proxy: function(req, res) {
+            uicUrl = url.parse(UIC_MICROWORLDS_URL)
+            req.url = req.url.replace(/^\/uic/,'')
+            console.log("PROXY "+req.url+" ==> "+UIC_MICROWORLDS_URL)
+            sail.server.proxy.proxyRequest(req, res, {
+                host: uicUrl.hostname,
+                port: uicUrl.port || 80
+            })
+        }
+    }
+)
+
 sail.server.start(8000)
